0f7d692554a72b3572da138b48c455c7b7feb04e
Renan Verissimo de vasconcelos <renanverissimo@uol.com.br>
2018-08-16 17:42:20 -0300

    Fix no calculo dos bits da url de decodificação

--- app/decodificador.js
+++ app/decodificador.js
@@ -6,7 +6,7 @@ var decodificar = function(string) {
       return reject();
     }
     
-    for(let i = 52; i < string.length; i += 4) {
+    for(let i = 0; i < string.length; i += 4) {
       let char = string[i] + string[i+1] + string[i+2] + string[i+3];
       char = char - 1000;
       stringDecodificada += String.fromCharCode(char);
